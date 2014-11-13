6d6725af7e642e21e0cc72fe2cb7d66a9c764d00
"Jorge Jimenez" <"Jorge Jimenez"@jorge.jimenez-HP-Compaq-8000-Elite-SFF-PC>
2014-11-13 13:18:59 -0600

    caso de sujeto sin vecinos

--- src/com/tralix/kata/gameoflife/GameOfLife.java
+++ src/com/tralix/kata/gameoflife/GameOfLife.java
@@ -1,6 +1,11 @@
 package com.tralix.kata.gameoflife;
 
-
 public class GameOfLife {
+
+    public static final int DEAD = 0;
+
+    public int nextOffspring(final int status, final int neighbors) {
+        return DEAD;
+    }
 
 }
--- test/com/tralix/kata/gameoflife/GameOfLifeTest.java
+++ test/com/tralix/kata/gameoflife/GameOfLifeTest.java
@@ -1,15 +1,22 @@
 package com.tralix.kata.gameoflife;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class GameOfLifeTest {
 
-    @Test
+    private GameOfLife game;
+
+    @Before
     public void setUp() throws Exception {
-        GameOfLife game = new GameOfLife();
-        assertNotNull(game);
+        game = new GameOfLife();
     }
 
+    @Test
+    public void givenLiveCellWithNoNeighbors_ret() throws Exception {
+        int result = game.nextOffspring(1, 0);
+        assertEquals(GameOfLife.DEAD, result);
+    }
 }
